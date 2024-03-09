87e66f95cd0d422c854969d9b5a6d35d2cf9a8e7
ajay.panthagani <ajay.panthagani@gojek.com>
2024-03-09 15:55:33 +0530

    feat: make recorder a provider

--- app/src/main/java/soundrecorder/resource/Recorder.java
+++ app/src/main/java/soundrecorder/resource/Recorder.java
@@ -1,17 +1,24 @@
 package soundrecorder.resource;
 
+import soundrecorder.interfaces.Provider;
+import soundrecorder.interfaces.Subscriber;
+
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Recorder {
+public class Recorder implements Provider<byte[]> {
     private final TargetDataLine line;
+    List<Subscriber<byte[]>> subscribers;
     private boolean isRecording = false;
     private byte[] audioByteStream;
 
     public Recorder(AudioFormat format) throws LineUnavailableException {
+        subscribers = new ArrayList<>();
         line = AudioSystem.getTargetDataLine(format);
         line.open(format);
     }
@@ -28,6 +35,7 @@ public class Recorder {
                 int bytesRead = line.read(buffer, 0, buffer.length);
                 outputStream.write(buffer, 0, bytesRead);
                 audioByteStream = outputStream.toByteArray();
+                notifyAllSubscribers(audioByteStream);
             }
         }
     }
@@ -40,5 +48,19 @@ public class Recorder {
 
     public byte[] getAudioByteStream() {
         return audioByteStream;
+    }
+
+    public void subscribe(Subscriber<byte[]> s) {
+        subscribers.add(s);
+    }
+
+    public void unsubscribe(Subscriber<byte[]> s) {
+        subscribers.remove(s);
+    }
+
+    public void notifyAllSubscribers(byte[] data) {
+        for (var subscriber : subscribers) {
+            subscriber.onNotify(data);
+        }
     }
 }
